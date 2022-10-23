7749051dde00be0f373c54ca6ce85553d55d828a
PaiNt <PaiNt@host.docker.internal>
2022-10-23 20:49:07 +1000

    fix

--- src/lab1_lexical_analysis/main.py
+++ src/lab1_lexical_analysis/main.py
@@ -59,6 +59,8 @@ class LexicalAnalyzerC:
         'CHAR',     # CHAR - CHAR constants state - Статус строковых констант
     ]
 
+    EXCEPT_CHARS = (' ', '\n', '\t', '')
+
     TABLE_SPEC_WORDS = {
         'char': 0,
         'double': 1,
@@ -133,20 +135,37 @@ class LexicalAnalyzerC:
     def __init__(self, program_filename: str):
         self.program_filename = program_filename
         self._current_state = 'S'
+        self._current_char = ''
+        self._current_linen = 0
+        self._current_сharn = 0
 
-        self._file_data = ''
+        self.__buffer = CharStack()
+        self.__ALL_registry = []
+
+        self._analysed_lines = CharQueue()
         with open(self.program_filename, 'rt', encoding='utf-8') as f:
-            self._file_data = f.read()
+            for line in f:
+                self._analysed_lines.add(line)
 
-        self._ALL_registry = []
+        self._current_line = CharQueue()
+
+    @property
+    def current_state(self):
+        return self._current_state
+
+    @current_state.setter
+    def _current_state(self, value):
+        if value not in self.STATES:
+            raise ValueError('Статус не из списка допустимых')
+        self._current_state = value
 
     @property
     def ALL_registry(self):
-        return self._ALL_registry
+        return self.__ALL_registry
 
     @ALL_registry.setter
-    def ALL_registry(self, value):
-        self._ALL_registry = value
+    def _ALL_registry(self, value):
+        self.__ALL_registry = value
 
     W_registry = property(lambda s: [x for x in s.ALL_registry if x.class_code == 'W'])
     N_registry = property(lambda s: [x for x in s.ALL_registry if x.class_code == 'N'])
@@ -190,42 +209,222 @@ class LexicalAnalyzerC:
     def __str__(self):
         return '\n'.join(self.get_table_rows_cols())
 
+    def add_buff(self, item, state=None):
+        if state:
+            self.current_state = state
+
+        self.__buffer.add(item)
+
+    def clear_buff(self, state=None):
+        if state:
+            self.current_state = state
+
+        self.__buffer.clear()
+
+    def read_buff(self, top=False, bottom=False):
+        if top:
+            return self.__buffer.top()
+        if bottom:
+            return self.__buffer.bottom()
+
+        self.__buffer.read()
+
+    @property
+    def is_buff_empty(self):
+        return self.__buffer.is_empty
+
     def run_analysis(self):
+        """
+        Начать анализ. Заполнить таблицы лексем.
+        """
+
+        while not self._analysed_lines.is_empty() or not self._current_line.is_empty():
+
+            if self.current_state == 'F':
+                break
+
+            self._current_line = CharQueue(self._analysed_lines.pop())
+            self._current_linen += 1
+
+            self._current_сharn = 0
+            self._current_char = self._current_line.pop()
+            self._current_сharn += 1
+
+            while self._current_char or not self.is_buff_empty:
+                if self.current_state == 'S':
+
+                    if self._current_char in self.EXCEPT_CHARS:
+                        pass
+
+                    elif self._current_char == '#':
+                        break
+
+                    elif self._current_char == '/':
+                        next_chapter = self.__current_line.pop()
+                        if next_chapter == '*':
+                            # self.__current_status.ID = 'multiline_comment'
+                            # заранее определяемся с возможной ошибкой
+                            self.override_status_and_or_message('multiline_comment', True, 'unterminated comment')
+                        elif next_chapter == '/':
+                            break
+                        else:
+                            # добавить /
+                            self.decide_what_to_do_with_lex('operations', '/')
+                            self.__current_chapter = next_chapter
+                            continue
+
+                    elif self._current_char.isalpha():
+                        self.change_status_and_add_symbol_to_buffer('letters')
+
+                    elif self._current_char == '_':
+                        self.change_status_and_add_symbol_to_buffer('identifiers')
+
+                    elif self._current_char.isdigit():
+                        self.change_status_and_add_symbol_to_buffer('integer_numbers')
+
+                    elif self._current_char == '.':
+                        self.change_status_and_add_symbol_to_buffer('real_numbers')
+
+                    elif self._current_char == "'" or self._current_char == '"':
+                        self.__stop_litter_for_character_constants = self.__current_chapter
+                        self.override_status_and_or_message('character_constants',
+                                                            True,
+                                                            'Символьная константа не имеет закрывающей кавычки!')
+
+                    elif self._current_char in self.MULTY_CHAR_OPERATIONS:
+                        self.change_status_and_add_symbol_to_buffer('two_litters_operations')
+
+                    elif self._current_char in self.SINGLE_CHAR_OPERATIONS:  # ['/', '^', ':', '%']
+                        self.decide_what_to_do_with_lex('operations', self.__current_chapter)
+
+                    elif self._current_char in self.TABLE_SEPARATORS:
+                        self.decide_what_to_do_with_lex('separators', self.__current_chapter)
+
+                    else:
+                        self.override_status_and_or_message('error', True,
+                                                            f"Символ {repr(self.__current_chapter)} не может быть "
+                                                            f"лексемой в данном контексте ({self.__current_status.ID})")
+                        break
+
+                else:
+
+                    if self.__current_status.ID == 'multiline_comment':
+                        if self.__current_chapter == '*':
+                            next_chapter = self.__current_line.pop()
+                            if next_chapter == '/':
+                                self.override_status_and_or_message('start')
+
+                    elif self.__current_status.ID == 'letters' or self.__current_status.ID == 'identifiers':
+                        if self.__syntax.is_digits(self.__current_chapter) or self.__current_chapter == '_':
+                            self.change_status_and_add_symbol_to_buffer('identifiers')
+                        elif self.__syntax.is_letters(self.__current_chapter):
+                            self.__buffer.add(self.__current_chapter)
+                        elif self.__syntax.is_skip_separators(self.__current_chapter) or \
+                                self.__syntax.is_separators(self.__current_chapter) or \
+                                self.__syntax.is_in_two_litters_operations(self.__current_chapter) or \
+                                self.__syntax.is_only_one_litters_operations(self.__current_chapter):
+                            if self.__current_status.ID == 'letters':
+                                self.decide_what_to_do_with_lex('functional_words')
+                            else:
+                                self.decide_what_to_do_with_lex('identifiers')
+                            continue
+                        else:
+                            self.override_status_and_or_message('error', True,
+                                                                f"Символ {repr(self.__current_chapter)} не может быть "
+                                                                f"лексемой в данном контексте "
+                                                                f"({self.__current_status.ID})")
+                            break
+
+                    elif self.__current_status.ID == 'integer_numbers' or self.__current_status.ID == 'real_numbers':
+                        if self.__syntax.is_digits(self.__current_chapter):
+                            self.__buffer.add(self.__current_chapter)
+                        elif self.__current_chapter == '.' and self.__current_status.ID == 'integer_numbers':
+                            self.change_status_and_add_symbol_to_buffer('real_numbers')
+                        elif self.__syntax.is_skip_separators(self.__current_chapter) or \
+                                self.__syntax.is_separators(self.__current_chapter) or \
+                                self.__syntax.is_in_two_litters_operations(self.__current_chapter) or \
+                                self.__syntax.is_only_one_litters_operations(self.__current_chapter):
+                            self.decide_what_to_do_with_lex('numeric_constants')
+                            continue
+                        else:
+                            self.override_status_and_or_message('error', True,
+                                                                f"Символ {repr(self.__current_chapter)} не может быть "
+                                                                f"лексемой в данном контексте "
+                                                                f"({self.__current_status.ID})")
+                            break
+
+                    elif self.__current_status.ID == 'character_constants':
+
+                        if self.__current_chapter == '\\':
+                            next_chapter = self.__current_line.pop()
+                            if next_chapter == '\n':
+                                break
+                            elif next_chapter == 'n':
+                                self.__buffer.add('\n')
+                            elif next_chapter == 't':
+                                self.__buffer.add('\t')
+                            else:  # любой другой
+                                self.__buffer.add(next_chapter)
+                        elif (self.raw_code.is_empty() and self.__current_line.is_empty() and \
+                              not self.__current_chapter) or self.__current_chapter == '\n':
+                            self.override_status_and_or_message('error', clear_message=False)
+                            self.__buffer.clear()
+                            break
+                        elif self.__current_chapter == self.__stop_litter_for_character_constants:
+                            if self.__stop_litter_for_character_constants == '"':
+                                self.decide_what_to_do_with_lex('character_constants')
+                            else:  # "'"
+                                item = self.__buffer.show_all_as_str()
+                                if len(item) <= 1:
+                                    self.decide_what_to_do_with_lex('character_constants', item)
+                                else:
+                                    self.decide_what_to_do_with_lex('character_constants', item[-1:])
+                                    print('warning: character constant too long for its type')
+                        else:
+                            self.__buffer.add(self.__current_chapter)
+
+                    elif self.__current_status.ID == 'two_litters_operations':
+                        join_buffer = self.__buffer.items[0] + self.__current_chapter
+                        if self.decide_what_to_do_with_lex('operations', join_buffer) == \
+                                'is_not_two_litters_operations':
+                            if self.__buffer.items[0] in self.__syntax.const_tables_of_lex['operations']:  # |
+                                self.decide_what_to_do_with_lex('operations')
+                                continue
+                            else:  # не это однолитерная операция
+                                self.override_status_and_or_message('error', True,
+                                                                    f"{repr(self.__buffer.items[0])} или "
+                                                                    f"{repr(join_buffer)} не является оператором!")
+                                break
+
+                self.__current_chapter = self.__current_line.pop()
+
         pass
 
 
 if __name__ == "__main__":
 
-    anal = LexicalAnalyzerC(r'M:\home\magistry2022\study.teor_pl\src\test_gtk\lab_1\data\input_code.c')
-
-    anal.add_lexem(Lexem("W", 1))
-
-    anal.add_lexem(Lexem("W", 1))
-
-    anal.add_lexem(Lexem("W", 1))
-
-    anal.add_lexem(Lexem("C", 1))
+    anal = LexicalAnalyzerC(r'M:\home\git\\magistry2022\study.teor_pl\src\test_gtk_artem\lab_1\data\input_code.c')
 
     sss = CharStack([1, 2, 3])
 
-    sss.add(4)
+    sss.add(10)
+    sss.add(20)
+    sss.add(30)
+    sss.add(40)
+
+    for elem in sss:
+        print(elem)
+
+    print("====")
 
     qqq = CharQueue([1, 2, 3])
 
-    qqq.add(4)
+    qqq.add(100)
+    qqq.add(200)
+    qqq.add(300)
+    qqq.add(400)
 
-    print(sss)
-
-    print(sss.pop())
-    print(sss.pop())
-    print(sss.pop())
-
-    print(qqq)
-
-    print(qqq.pop())
-    print(qqq.pop())
-    print(qqq.pop())
-
-    print("")
+    for elem in qqq:
+        print(elem)
 
     pass
--- src/shared_classes.py
+++ src/shared_classes.py
@@ -19,18 +19,20 @@ class _CharStackQueue:
             for x in initlist:
                 self.add(str(x))
 
+    @property
     def is_empty(self):
         if self._items and len(self._items):
             return False
         return True
 
     def pop(self):
-        if self.is_empty():
+        if self.is_empty:
             return ''
         else:
             return self._items.pop()
 
     def clear(self):
+        del self._items
         self._items = []
 
     def __len__(self):
@@ -45,18 +47,64 @@ class _CharStackQueue:
 class CharStack(_CharStackQueue):
     is_stack = True
 
+    def top(self):
+        try:
+            return self._items[-1]
+        except:
+            pass
+        return ''
+
+    def bottom(self):
+        try:
+            return self._items[0]
+        except:
+            pass
+        return ''
+
     def add(self, value):
         self._items.append(str(value))
 
-    def show_all_as_str(self):
+    def read(self):
         return ''.join(self._items)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        x = self.pop()
+        if x != '':
+            return x
+        raise StopIteration
 
 
 class CharQueue(_CharStackQueue):
     is_stack = False
 
+    def first(self):
+        try:
+            return self._items[-1]
+        except:
+            pass
+        return ''
+
+    def last(self):
+        try:
+            return self._items[0]
+        except:
+            pass
+        return ''
+
     def add(self, value):
         self._items.insert(0, str(value))
 
-    def show_all_as_str(self):
+    def read(self):
         return ''.join(reversed(self._items))
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        x = self.pop()
+        if x != '':
+            return x
+        raise StopIteration
