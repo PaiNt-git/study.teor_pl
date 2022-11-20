b40290bc64e4ada65bb7d48c7a3063a6a04628a5
ASZabavin <ASZabavin@pnu.edu.ru>
2022-11-21 09:53:28 +1000

    fix

--- src/lab2_postfix_notation/main.py
+++ src/lab2_postfix_notation/main.py
@@ -4,11 +4,12 @@ Created on 15 сент. 2022 г.
 @author: ASZabavin
 '''
 
+from collections import OrderedDict
+from itertools import groupby
+
 from shared_classes import CharStack, CharQueue
 
 from lab1_lexical_analysis.main import LexicalAnalyzerC
-from itertools import groupby
-from collections import OrderedDict
 
 
 class DijkstratorC():
@@ -26,7 +27,7 @@ class DijkstratorC():
             for lxorder in lexems:
                 yield prio, self.la_instance.get_reg_lexem_by_order(lxorder[0])
 
-    def get_priority_by_id(self, class_code: str, lex_id, lex_text=None):
+    def get_priority_by_lexemid(self, class_code: str, lex_id, lex_text=None):
         """
         Таблица приоритетов Дисктры для языка
 
@@ -41,13 +42,26 @@ class DijkstratorC():
 
         return 0
 
+    def get_priority_by_reg_lex_order(self, order: int):
+        lx = self.la_instance.get_reg_lexem_by_order(order)
+        return self.get_priority_by_lexemid(lx.class_code, lx.lex_id, lx.text)
+
     def __init__(self, la_instance: LexicalAnalyzerC):
         self.la_instance = la_instance
 
         if not len(la_instance.ALL_registry):
             la_instance.run_analysis()
-        self.TABLE_PRIORITY = {lx.order: self.get_priority_by_id(lx.class_code, lx.lex_id, lx.text) for lx in la_instance.ALL_registry}
+        self.TABLE_PRIORITY = {lx.order: self.get_priority_by_lexemid(lx.class_code, lx.lex_id, lx.text) for lx in la_instance.ALL_registry}
+
+        self._postfix_stack = CharStack()
+        self._output_string = ''
         pass
+
+    def run_postfix_notatization(self):
+        pass
+
+    def __str__(self):
+        return self._output_string
 
 
 if __name__ == "__main__":
